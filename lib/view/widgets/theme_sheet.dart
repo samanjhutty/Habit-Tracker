7743fea192813351360dadd9976c654a14fb8aea
Saman Jhutty <jhuttysaman@gmail.com>
2024-01-26 20:22:51 +0530

    fix: theme sheet overflow

--- lib/view/widgets/theme_sheet.dart
+++ lib/view/widgets/theme_sheet.dart
@@ -31,197 +31,202 @@ class _MyBottomSheetState extends State<MyBottomSheet>
       showDragHandle: true,
       onClosing: () {},
       builder: (context) => Column(
-        mainAxisSize: MainAxisSize.min,
         children: [
-          RadioListTile(
-            value: const Color(0xFFFB5B76),
-            groupValue: appThemeColor,
-            onChanged: (value) {
-              setState(() {
-                appThemeColor = value!;
-              });
-            },
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          Flexible(
+            child: ListView(
               children: [
-                const Text('Default'),
-                Container(
-                  decoration: BoxDecoration(
-                      color: scheme.secondary, shape: BoxShape.circle),
-                  padding: const EdgeInsets.all(4),
-                  child: const CircleAvatar(
-                    radius: 16,
-                    backgroundColor: Color(0xFFFB5B76),
+                RadioListTile(
+                  value: const Color(0xFFFB5B76),
+                  groupValue: appThemeColor,
+                  onChanged: (value) {
+                    setState(() {
+                      appThemeColor = value!;
+                    });
+                  },
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Default'),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: scheme.secondary, shape: BoxShape.circle),
+                        padding: const EdgeInsets.all(4),
+                        child: const CircleAvatar(
+                          radius: 16,
+                          backgroundColor: Color(0xFFFB5B76),
+                        ),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-          ),
-          RadioListTile(
-            value: const Color(0xFFB2FF59),
-            groupValue: appThemeColor,
-            onChanged: (value) {
-              setState(() {
-                appThemeColor = value!;
-              });
-            },
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('Light Green'),
-                Container(
-                  decoration: BoxDecoration(
-                      color: scheme.secondary, shape: BoxShape.circle),
-                  padding: const EdgeInsets.all(4),
-                  child: const CircleAvatar(
-                    radius: 16,
-                    backgroundColor: Colors.lightGreenAccent,
+                ),
+                RadioListTile(
+                  value: const Color(0xFFB2FF59),
+                  groupValue: appThemeColor,
+                  onChanged: (value) {
+                    setState(() {
+                      appThemeColor = value!;
+                    });
+                  },
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Light Green'),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: scheme.secondary, shape: BoxShape.circle),
+                        padding: const EdgeInsets.all(4),
+                        child: const CircleAvatar(
+                          radius: 16,
+                          backgroundColor: Colors.lightGreenAccent,
+                        ),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-          ),
-          RadioListTile(
-            value: const Color(0xFF40C4FF),
-            groupValue: appThemeColor,
-            onChanged: (value) {
-              setState(() {
-                appThemeColor = value!;
-              });
-            },
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('Light Blue'),
-                Container(
-                  decoration: BoxDecoration(
-                      color: scheme.secondary, shape: BoxShape.circle),
-                  padding: const EdgeInsets.all(4),
-                  child: const CircleAvatar(
-                    radius: 16,
-                    backgroundColor: Colors.lightBlueAccent,
+                ),
+                RadioListTile(
+                  value: const Color(0xFF40C4FF),
+                  groupValue: appThemeColor,
+                  onChanged: (value) {
+                    setState(() {
+                      appThemeColor = value!;
+                    });
+                  },
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Light Blue'),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: scheme.secondary, shape: BoxShape.circle),
+                        padding: const EdgeInsets.all(4),
+                        child: const CircleAvatar(
+                          radius: 16,
+                          backgroundColor: Colors.lightBlueAccent,
+                        ),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-          ),
-          RadioListTile(
-            value: const Color(0xFFE040FB),
-            groupValue: appThemeColor,
-            onChanged: (value) {
-              setState(() {
-                appThemeColor = value!;
-              });
-            },
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('Purple'),
-                Container(
-                  decoration: BoxDecoration(
-                      color: scheme.secondary, shape: BoxShape.circle),
-                  padding: const EdgeInsets.all(4),
-                  child: const CircleAvatar(
-                    radius: 16,
-                    backgroundColor: Color(0xFFE040FB),
+                ),
+                RadioListTile(
+                  value: const Color(0xFFE040FB),
+                  groupValue: appThemeColor,
+                  onChanged: (value) {
+                    setState(() {
+                      appThemeColor = value!;
+                    });
+                  },
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Purple'),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: scheme.secondary, shape: BoxShape.circle),
+                        padding: const EdgeInsets.all(4),
+                        child: const CircleAvatar(
+                          radius: 16,
+                          backgroundColor: Color(0xFFE040FB),
+                        ),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-          ),
-          RadioListTile(
-            value: const Color(0xFF64FFDA),
-            groupValue: appThemeColor,
-            onChanged: (value) {
-              setState(() {
-                appThemeColor = value!;
-              });
-            },
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('Teal'),
-                Container(
-                  decoration: BoxDecoration(
-                      color: scheme.secondary, shape: BoxShape.circle),
-                  padding: const EdgeInsets.all(4),
-                  child: const CircleAvatar(
-                    radius: 16,
-                    backgroundColor: Color(0xFF64FFDA),
+                ),
+                RadioListTile(
+                  value: const Color(0xFF64FFDA),
+                  groupValue: appThemeColor,
+                  onChanged: (value) {
+                    setState(() {
+                      appThemeColor = value!;
+                    });
+                  },
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Teal'),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: scheme.secondary, shape: BoxShape.circle),
+                        padding: const EdgeInsets.all(4),
+                        child: const CircleAvatar(
+                          radius: 16,
+                          backgroundColor: Color(0xFF64FFDA),
+                        ),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-          ),
-          RadioListTile(
-            value: const Color(0xFF536DFE),
-            groupValue: appThemeColor,
-            onChanged: (value) {
-              setState(() {
-                appThemeColor = value!;
-              });
-            },
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('Indigo'),
-                Container(
-                  decoration: BoxDecoration(
-                      color: scheme.secondary, shape: BoxShape.circle),
-                  padding: const EdgeInsets.all(4),
-                  child: const CircleAvatar(
-                    radius: 16,
-                    backgroundColor: Color(0xFF536DFE),
+                ),
+                RadioListTile(
+                  value: const Color(0xFF536DFE),
+                  groupValue: appThemeColor,
+                  onChanged: (value) {
+                    setState(() {
+                      appThemeColor = value!;
+                    });
+                  },
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Indigo'),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: scheme.secondary, shape: BoxShape.circle),
+                        padding: const EdgeInsets.all(4),
+                        child: const CircleAvatar(
+                          radius: 16,
+                          backgroundColor: Color(0xFF536DFE),
+                        ),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-          ),
-          RadioListTile(
-            value: const Color(0xFFFFD740),
-            groupValue: appThemeColor,
-            onChanged: (value) {
-              setState(() {
-                appThemeColor = value!;
-              });
-            },
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('Amber'),
-                Container(
-                  decoration: BoxDecoration(
-                      color: scheme.secondary, shape: BoxShape.circle),
-                  padding: const EdgeInsets.all(4),
-                  child: const CircleAvatar(
-                    radius: 16,
-                    backgroundColor: Color(0xFFFFD740),
+                ),
+                RadioListTile(
+                  value: const Color(0xFFFFD740),
+                  groupValue: appThemeColor,
+                  onChanged: (value) {
+                    setState(() {
+                      appThemeColor = value!;
+                    });
+                  },
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Amber'),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: scheme.secondary, shape: BoxShape.circle),
+                        padding: const EdgeInsets.all(4),
+                        child: const CircleAvatar(
+                          radius: 16,
+                          backgroundColor: Color(0xFFFFD740),
+                        ),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-          ),
-          RadioListTile(
-            value: const Color(0xFFFF6E40),
-            groupValue: appThemeColor,
-            onChanged: (value) {
-              setState(() {
-                appThemeColor = value!;
-              });
-            },
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('Deep Orange'),
-                Container(
-                  decoration: BoxDecoration(
-                      color: scheme.secondary, shape: BoxShape.circle),
-                  padding: const EdgeInsets.all(4),
-                  child: const CircleAvatar(
-                    radius: 16,
-                    backgroundColor: Color(0xFFFF6E40),
+                ),
+                RadioListTile(
+                  value: const Color(0xFFFF6E40),
+                  groupValue: appThemeColor,
+                  onChanged: (value) {
+                    setState(() {
+                      appThemeColor = value!;
+                    });
+                  },
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Deep Orange'),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: scheme.secondary, shape: BoxShape.circle),
+                        padding: const EdgeInsets.all(4),
+                        child: const CircleAvatar(
+                          radius: 16,
+                          backgroundColor: Color(0xFFFF6E40),
+                        ),
+                      )
+                    ],
                   ),
-                )
+                ),
               ],
             ),
           ),
